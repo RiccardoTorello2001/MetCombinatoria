4b7e0539a09e79bb12762831aec1fd8608d7e7ef
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-03-06 09:11:41 +0100

    ho iniziato ad risolvere il fattoriale

---
+++ CalcoliCombinatori.Test/CalcoliCombinatori_Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EquazioniLibrary;
+
+namespace CalcoliCombinatori.Test
+{
+    [TestClass]
+    public class CalcoliCombinatori_Test
+    {
+        [DataTestMethod]
+        [DataRow(5,120)]
+        [DataRow(0, 1)]
+        [DataRow(1, 1)]
+        [DataRow(10, 3628800)]
+        [DataRow(20, 2432902008176640000)]
+        
+        [TestMethod]
+        public void TestFatt1(int num, int Fact)
+        {
+            long obt = EquazioniLibrary.CalcoliCombinatori.Fattoriale(num);
+            Assert.AreEqual(obt,Fact);
+        }
+
+    }
+}
---
+++ EquazioniLibrary/CalcoliCombinatori.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquazioniLibrary
+{
+    public class CalcoliCombinatori
+    {
+        
+        public static long Fattoriale(int num)
+        {
+            long fattoriale = 1;
+            if (num != 0)
+            {
+                for (int i = 1; i < num; i++)
+                {
+                    fattoriale = fattoriale * i;
+                }
+            }
+            else
+            {
+                fattoriale = 1;
+            }
+            return fattoriale;
+        }
+    }
+}
